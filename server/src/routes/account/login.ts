2f29ad10310dda497820b633994f6e0ed084893c
Franze, Mario <mfranze@codingden.de>
2020-03-26 22:41:06 +0100

    Removed basic logger

--- server/src/routes/account/login.ts
+++ server/src/routes/account/login.ts
@@ -5,8 +5,7 @@ const router: Router = Router();
 
 router.post('/login', (req: Request, res: Response) => {
 	// Import the basic & error logger
-	/* const basicLogger = loggers.get('basic-logger');
-	const errorLogger = loggers.get('error-logger'); */
+	// const errorLogger = loggers.get('error-logger');
 });
 
 export const AccountLoginController: Router = router;
