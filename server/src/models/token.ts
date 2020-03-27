fd49378ddbde053e9a1ee19dff0ff52bf4cd92e1
Franze, Mario <mfranze@codingden.de>
2020-03-27 14:08:03 +0100

    Initial commit

---
+++ server/src/models/token.ts
@@ -0,0 +1,27 @@
+import {
+	Table,
+	Column,
+	Default,
+	Model,
+	ForeignKey,
+	BelongsTo,
+} from 'sequelize-typescript';
+import { literal } from 'sequelize';
+import { Account } from './account';
+
+@Table
+export class Token extends Model<Token> {
+	@Column
+	token!: string;
+
+	@Default(literal('CURRENT_TIMESTAMP'))
+	@Column
+	validUntil!: Date;
+
+	@ForeignKey(() => Account)
+	@Column
+	createdBy!: number;
+
+	@BelongsTo(() => Account)
+	createdByID!: number;
+}
